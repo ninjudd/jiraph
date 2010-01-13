d20f30c21b0ae5172dbcd98a218d9efcb54f5714
Justin Balthrop <justin@geni.com>
2010-01-13 11:06:14 -0800

    rename vertices to nodes. store node references in steps.

--- src/jiraph.clj
+++ src/jiraph.clj
@@ -3,36 +3,36 @@
   (:use cupboard.bdb.je)
   (:use clojure.contrib.java-utils))
 
-(defclass Graph  :env :vertices-db :edges-db)
-(defclass Vertex :id :type)
-(defclass Edge   :from-id :to-id :type)
+(defclass Graph :env :nodes-db :edges-db)
+(defclass Node  :id :type)
+(defclass Edge  :from-id :to-id :type)
 
 (defn open-graph [path]
-  (let [env         (db-env-open (file path) :allow-create true :transactional true)
-        vertices-db (db-open env "vertices"  :allow-create true :transactional true)
-        edges-db    (db-open env "edges"     :allow-create true :transactional true)]
-    (Graph [env vertices-db edges-db])))
+  (let [env      (db-env-open (file path) :allow-create true :transactional true)
+        nodes-db (db-open env "nodes"     :allow-create true :transactional true)
+        edges-db (db-open env "edges"     :allow-create true :transactional true)]
+    (Graph [env nodes-db edges-db])))
 
-(defn add-vertex! [graph & args]
-  (let [vertex (Vertex args)]
-    (db-put (graph :vertices-db) 
-            (:id vertex) vertex 
+(defn add-node! [graph & args]
+  (let [node (Node args)]
+    (db-put (graph :nodes-db) 
+            (:id node) node 
             :no-overwrite true)))
 
-(defn delete-vertex! [graph id]
-  (db-delete (graph :vertices-db) id))
+(defn delete-node! [graph id]
+  (db-delete (graph :nodes-db) id))
 
-(defn get-vertex [graph id]
-  (let [[key vertex] (db-get (graph :vertices-db) id)]
-    vertex))
+(defn get-node [graph id]
+  (let [[key node] (db-get (graph :nodes-db) id)]
+    node))
 
-(defn assoc-vertex! [graph & args] ; modify an existing vertex
-  (let [attrs  (Vertex args)
+(defn assoc-node! [graph & args] ; modify an existing node
+  (let [attrs  (Node args)
         id     (:id attrs)
-        vertex (get-vertex graph id)]
-    (if vertex
-      (db-put (graph :vertices-db)
-              id (merge vertex attrs))
+        node (get-node graph id)]
+    (if node
+      (db-put (graph :nodes-db)
+              id (merge node attrs))
       nil)))
 
 (defn edge-key [edge]
@@ -73,11 +73,12 @@
               key (merge edge attrs))
       nil)))
 
-(defclass Walk :focus-id :steps :vertices)
-(defclass Step :id :source :edge)
+(defclass Walk :graph :focus-id :steps :nodes)
+(defclass Step :source :edge :node)
 
-(defn walk-vertex [walk id]
-  (get-in walk [:vertices id]))
+(defn walk-node [walk id]
+  (or (get-in walk [:nodes id])
+      (get-node (walk :graph) id)))
 
 (defn walk-step
   ([walk edge]
@@ -88,27 +89,32 @@
 (defn walked? [walk edge]
   (not (nil? (walk-step walk edge))))
 
-(defn follow [graph id step]
-  (map #(Step [id step %]) (get-edges graph id)))
+(defn follow [walk from-id step]
+  (let [graph (walk :graph)]
+    (map (fn [edge]
+           (let [to-id (edge :to-id)
+                 node  (walk-node walk to-id)]
+             (Step [step edge node])))
+         (get-edges graph from-id))))
 
 (defn full-walk [graph focus-id]
-  (let [vertex (get-vertex graph focus-id)]
-    (if (nil? vertex)
+  (let [node (get-node graph focus-id)]
+    (if (nil? node)
       nil
-      (loop [walk      (Walk [focus-id {} {focus-id vertex}])
-             to-follow (queue (follow graph focus-id nil))]
+      (loop [walk      (Walk [graph focus-id {} {focus-id node}])
+             to-follow (queue (follow walk focus-id nil))]
         (if (empty? to-follow)
           walk
-          (let [step      (first to-follow)
-                edge      (step :edge)
-                from-id   (edge :from-id)
-                to-id     (edge :to-id)
-                type      (edge :type)
-                vertex    (or (walk-vertex walk to-id) (get-vertex graph to-id))
+          (let [step    (first to-follow)
+                edge    (step :edge)
+                node    (step :node)
+                from-id (edge :from-id)
+                to-id   (edge :to-id)
+                type    (edge :type)
                 to-follow (pop to-follow)]
             (if (walked? walk edge)
               (recur walk to-follow)
               (recur (-> walk
                          (assoc-in [:steps to-id from-id type] step)
-                         (assoc-in [:vertices to-id] vertex))
-                     (into to-follow (follow graph to-id step))))))))))
+                         (assoc-in [:nodes to-id] node))
+                     (into to-follow (follow walk to-id step))))))))))
