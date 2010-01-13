d13eaa7f551be51b4065c26b028c980e21e93a51
Justin Balthrop <justin@geni.com>
2010-01-12 18:17:13 -0800

    add basic full walk and switch to using defclass

--- src/jiraph.clj
+++ src/jiraph.clj
@@ -1,26 +1,20 @@
 (ns jiraph
-  (:use cupboard.bdb.je cupboard.utils)
+  (:use ninjudd.utils)
+  (:use cupboard.bdb.je)
   (:use clojure.contrib.java-utils))
 
-(defstruct graph        :env :vertices-db :edges-db)
-(defstruct graph-vertex :id :type)
-(defstruct graph-edge   :from-id :to-id :type)
-
-(defn- to-struct [s args]
-  (if (and (sequential? args)
-           (= (count args) 1)
-           (map? (first args)))
-    (merge (struct-map s) (first args))
-    (apply struct-map s args)))
+(defclass Graph  :env :vertices-db :edges-db)
+(defclass Vertex :id :type)
+(defclass Edge   :from-id :to-id :type)
 
 (defn open-graph [path]
   (let [env         (db-env-open (file path) :allow-create true :transactional true)
         vertices-db (db-open env "vertices"  :allow-create true :transactional true)
         edges-db    (db-open env "edges"     :allow-create true :transactional true)]
-    (struct graph env vertices-db edges-db)))
+    (Graph [env vertices-db edges-db])))
 
 (defn add-vertex! [graph & args]
-  (let [vertex (to-struct graph-vertex args)]
+  (let [vertex (Vertex args)]
     (db-put (graph :vertices-db) 
             (:id vertex) vertex 
             :no-overwrite true)))
@@ -28,14 +22,14 @@
 (defn delete-vertex! [graph id]
   (db-delete (graph :vertices-db) id))
 
-(defn vertex [graph id]
+(defn get-vertex [graph id]
   (let [[key vertex] (db-get (graph :vertices-db) id)]
     vertex))
 
-(defn assoc-vertex! [graph & args]
-  (let [attrs  (to-struct graph-vertex args)
+(defn assoc-vertex! [graph & args] ; modify an existing vertex
+  (let [attrs  (Vertex args)
         id     (:id attrs)
-        vertex (vertex graph id)]
+        vertex (get-vertex graph id)]
     (if vertex
       (db-put (graph :vertices-db)
               id (merge vertex attrs))
@@ -45,7 +39,7 @@
   (map edge '(:from-id :type :to-id)))
 
 (defn add-edge! [graph & args]
-  (let [edge (to-struct graph-edge args)]
+  (let [edge (Edge args)]
     (db-put (graph :edges-db)
             (edge-key edge) edge 
             :no-overwrite true)))
@@ -53,27 +47,68 @@
 (defn delete-edge! [graph from-id to-id type]
   (db-delete (graph :edges-db) (list from-id type to-id)))
 
-(defn- edges* [graph start-key]
-  (let [n      (count start-key)
-        cursor (db-cursor-open (graph :edges-db))]
-    (loop [[key value] (db-cursor-search cursor start-key)
-           edges       []]
-      (if (= (take n key) (take n start-key))
-        (recur (db-cursor-next cursor) (conj edges value))
-        edges))))
+(defn- get-edges* [graph key]
+  (let [n   (count key)
+        key (into (repeat (- 3 n) nil) key)] ; must be length 3
+    (with-db-cursor [cursor (graph :edges-db)]
+      (loop [[k v] (db-cursor-search cursor key)
+             edges []]
+        (if (= (take n k) (take n key))
+          (recur (db-cursor-next cursor) (conj edges v))
+          edges)))))
 
-(defn edge [graph from-id to-id type]
-  (first (edges* graph (list from-id type to-id))))
+(defn get-edge [graph from-id to-id type]
+  (first (get-edges* graph (list from-id type to-id nil))))
 
-(defn edges
-  ([graph from-id type] (edges* graph (list from-id type)))
-  ([graph from-id]      (edges* graph (list from-id))))
+(defn get-edges
+  ([graph from-id type] (get-edges* graph (list from-id type)))
+  ([graph from-id]      (get-edges* graph (list from-id))))
 
-(defn assoc-edge! [graph & args]
-  (let [attrs (to-struct graph-edge args)
+(defn assoc-edge! [graph & args] ; modify an existing edge
+  (let [attrs (Edge args)
         key   (edge-key attrs)
-        edge  (first (edges* graph key))]
+        edge  (first (get-edges* graph key))]
     (if edge
       (db-put (graph :edges-db)
               key (merge edge attrs))
       nil)))
+
+(defclass Walk :focus-id :steps :vertices)
+(defclass Step :id :source :edge)
+
+(defn walk-vertex [walk id]
+  (get-in walk [:vertices id]))
+
+(defn walk-step
+  ([walk edge]
+     (apply walk-step walk (map edge '(:to-id :from-id :type))))
+  ([walk from-id to-id type]
+     (get-in walk [:steps to-id from-id type])))
+
+(defn walked? [walk edge]
+  (not (nil? (walk-step walk edge))))
+
+(defn follow [graph id step]
+  (map #(Step [id step %]) (get-edges graph id)))
+
+(defn full-walk [graph focus-id]
+  (let [vertex (get-vertex graph focus-id)]
+    (if (nil? vertex)
+      nil
+      (loop [walk      (Walk [focus-id {} {focus-id vertex}])
+             to-follow (queue (follow graph focus-id nil))]
+        (if (empty? to-follow)
+          walk
+          (let [step      (first to-follow)
+                edge      (step :edge)
+                from-id   (edge :from-id)
+                to-id     (edge :to-id)
+                type      (edge :type)
+                vertex    (or (walk-vertex walk to-id) (get-vertex graph to-id))
+                to-follow (pop to-follow)]
+            (if (walked? walk edge)
+              (recur walk to-follow)
+              (recur (-> walk
+                         (assoc-in [:steps to-id from-id type] step)
+                         (assoc-in [:vertices to-id] vertex))
+                     (into to-follow (follow graph to-id step))))))))))
---
+++ src/ninjudd/utils.clj
@@ -0,0 +1,25 @@
+(ns ninjudd.utils)
+
+(defmacro let-if [test then-bindings else-bindings & body]
+  `(if ~test
+     (let ~then-bindings
+       ~@body)
+     (let ~else-bindings
+       ~@body)))
+
+(defmacro defclass [class & fields]
+  `(let [type#   (keyword (name (quote ~class)))
+         struct# (create-struct ~@fields)]
+     (defn ~class [& args#]
+       (let [instance# (if (= (count args#) 1)
+                         (let [attrs# (first args#)]
+                           (cond (map? attrs#)    (merge (struct-map struct#) attrs#)
+                                 (vector? attrs#) (apply struct struct# attrs#)
+                                 (seq?    attrs#) (apply struct-map struct# attrs#)
+                                 :else (throw (IllegalArgumentException. "single arg must be map or vector"))))
+                         (apply struct-map struct# args#))]
+         (with-meta instance# {:type type#})))))
+
+(defn queue 
+  ([]    clojure.lang.PersistentQueue/EMPTY)
+  ([seq] (into clojure.lang.PersistentQueue/EMPTY seq)))
