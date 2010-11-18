12b31baf2cb7e857c46209ad815246642d8afa61
lance bradley <l@ncebradley.org>
2010-11-17 18:15:11 -0800

    update protobuf dependency

--- project.clj
+++ project.clj
@@ -3,6 +3,6 @@
   :dependencies [[clojure "1.2.0"]
                  [tokyocabinet "1.23-SNAPSHOT"]
                  [clojure-useful "0.3.0-SNAPSHOT"]]
-  :dev-dependencies [[clojure-protobuf "0.2.11"]
+  :dev-dependencies [[clojure-protobuf "0.3.0-SNAPSHOT"]
                      [cake-autodoc "0.0.1-SNAPSHOT"]]
   :tasks [protobuf.tasks cake-autodoc.tasks])
