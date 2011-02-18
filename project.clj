2ff4df7a0d700fef76fdbc484290662d52865a2a
ninjudd <git@justinbalthrop.com>
2011-02-18 14:08:58 -0800

    update protobuf

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject jiraph "0.5.6"
+(defproject jiraph "0.5.7-SNAPSHOT"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.2.0"]
                  [tokyocabinet "1.23-SNAPSHOT"]
                  [clojure-useful "0.3.1"]]
-  :dev-dependencies [[clojure-protobuf "0.3.2"]
+  :dev-dependencies [[clojure-protobuf "0.3.4-SNAPSHOT"]
                      [cake-autodoc "0.0.1-SNAPSHOT"]]
   :tasks [protobuf.tasks cake-autodoc.tasks])
