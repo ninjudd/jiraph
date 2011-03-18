eb01ebd6686094ae05afb0650010c36becd68580
ninjudd <git@justinbalthrop.com>
2011-03-18 12:22:27 -0700

    version 0.6.0

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject jiraph "0.6.0-SNAPSHOT"
+(defproject jiraph "0.6.0"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.2.0"]
-                 [clojure-useful "0.3.2-SNAPSHOT"]
-                 [ruminate "0.5.0-SNAPSHOT"]
-                 [retro "0.5.0-SNAPSHOT"]]
-  :dev-dependencies [[clojure-protobuf "0.3.4-SNAPSHOT"]]
+                 [clojure-useful "0.3.2"]
+                 [ruminate "0.5.0"]
+                 [retro "0.5.0"]]
+  :dev-dependencies [[clojure-protobuf "0.3.4"]]
   :tasks [protobuf.tasks])
