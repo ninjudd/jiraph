9a7be16af17fd3d4fcb15fdf533e3532fed19336
ninjudd <git@justinbalthrop.com>
2011-03-18 20:00:42 -0700

    add mlock support for tokyocabinet

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject jiraph "0.6.0"
+(defproject jiraph "0.6.1-SNAPSHOT"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.2.0"]
                  [clojure-useful "0.3.2"]
-                 [ruminate "0.5.0"]
+                 [ruminate "0.5.1-SNAPSHOT"]
                  [retro "0.5.0"]]
   :dev-dependencies [[clojure-protobuf "0.3.4"]]
   :tasks [protobuf.tasks])
