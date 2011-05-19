19b04d8eb5627d3b7bea18d014483690b0e30bc4
ninjudd <git@justinbalthrop.com>
2011-05-18 18:48:54 -0700

    version 0.6.1. update masai and protobuf

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
-(defproject jiraph "0.6.1-SNAPSHOT"
+(defproject jiraph "0.6.1"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.2.0"]
                  [clojure-useful "0.3.8"]
-                 [masai "0.5.1-SNAPSHOT"]
+                 [masai "0.5.1"]
                  [retro "0.5.0"]]
-  :dev-dependencies [[clojure-protobuf "0.4.0-SNAPSHOT"]]
+  :dev-dependencies [[clojure-protobuf "0.4.0"]
+                     [tokyocabinet "1.24.1-SNAPSHOT"]]
   :tasks [protobuf.tasks])
