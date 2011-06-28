a3d20a7ab432327b3ecdfee21553df4bacedda5d
Alan Malloy <alan@malloys.org>
2011-06-28 12:52:00 -0700

    Depend on ego snapshot

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [masai "0.5.1"]
                  [cereal "0.1.3"]
                  [retro "0.5.0"]
-                 [ego "0.1.0"]]
+                 [ego "0.1.1-SNAPSHOT"]]
   :dev-dependencies [[clojure-protobuf "0.4.5"]
                      [tokyocabinet "1.24.1-SNAPSHOT"]]
   :tasks [protobuf.tasks])
