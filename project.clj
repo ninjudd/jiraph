408772e92bd6569abddcc6d4b15426e229395b6c
Justin Balthrop <git@justinbalthrop.com>
2011-07-11 12:44:05 -0700

    update to non-snapshot deps

--- project.clj
+++ project.clj
@@ -2,10 +2,10 @@
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.2.0"]
                  [useful "0.5.0"]
-                 [masai "0.5.2-SNAPSHOT"]
-                 [cereal "0.1.5-SNAPSHOT"]
+                 [masai "0.5.2"]
+                 [cereal "0.1.5"]
                  [retro "0.5.0"]
                  [ego "0.1.1"]]
-  :dev-dependencies [[clojure-protobuf "0.4.7-SNAPSHOT"]
+  :dev-dependencies [[protobuf "0.5.0-alpha1"]
                      [tokyocabinet "1.24.1-SNAPSHOT"]]
   :tasks [protobuf.tasks])
