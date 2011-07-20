9e745a9c37d7d54c232d374cc1ca134ee57f6c14
Anthony Grimes <i@raynes.me>
2011-07-19 19:01:20 -0500

    Added cake-marginalia.

--- project.clj
+++ project.clj
@@ -7,5 +7,6 @@
                  [retro "0.6.0-alpha1"]
                  [ego "0.1.1"]]
   :dev-dependencies [[protobuf "0.5.0-alpha1"]
-                     [tokyocabinet "1.24.1-SNAPSHOT"]]
-  :tasks [protobuf.tasks])
+                     [tokyocabinet "1.24.1-SNAPSHOT"]
+                     [org.clojars.flatland/cake-marginalia "0.6.1"]]
+  :tasks [protobuf.tasks cake-marginalia.tasks])
