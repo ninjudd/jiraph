791ca2fdd556b2e7aa3d546b5861c5ba27d214ff
Alan Malloy <alan@malloys.org>
2011-08-01 17:28:20 -0700

    Depending on new version of useful

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
-(defproject jiraph "0.7.0-alpha5"
+(defproject jiraph "0.7.0-alpha6"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.5.0"]
+                 [useful "0.5.4"]
                  [masai "0.5.2"]
                  [cereal "0.1.5"]
                  [retro "0.6.0-alpha1"]
