520c37d92541a0625dde72d6fc605c13ac3b888f
Justin Balthrop <git@justinbalthrop.com>
2011-08-26 18:57:28 -0700

    bump ego

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
                  [masai "0.5.3"]
                  [cereal "0.1.5"]
                  [retro "0.6.0-alpha1"]
-                 [ego "0.1.1"]]
+                 [ego "0.1.5"]]
   :dev-dependencies [[protobuf "0.5.0-alpha1"]
                      [tokyocabinet "1.24.1-SNAPSHOT"]
                      [org.clojars.flatland/cake-marginalia "0.6.3"]]
