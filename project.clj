9c6d63bd02b8fe5ad075b9908f4c3738b943cf35
Justin Balthrop <git@justinbalthrop.com>
2011-08-26 18:53:01 -0700

    bump deps

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject jiraph "0.7.0-alpha11"
+(defproject jiraph "0.7.0-alpha12"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.6.0"]
-                 [masai "0.5.2"]
+                 [useful "0.7.0-alpha3"]
+                 [masai "0.5.3"]
                  [cereal "0.1.5"]
                  [retro "0.6.0-alpha1"]
                  [ego "0.1.1"]]
   :dev-dependencies [[protobuf "0.5.0-alpha1"]
                      [tokyocabinet "1.24.1-SNAPSHOT"]
-                     [org.clojars.flatland/cake-marginalia "0.6.2"]]
+                     [org.clojars.flatland/cake-marginalia "0.6.3"]]
   :tasks [protobuf.tasks cake-marginalia.tasks])
