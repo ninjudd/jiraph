572907da9e0b1b7fb74ee98e574f71f324abd22d
Alan Malloy <alan@malloys.org>
2011-09-15 19:59:14 -0700

    More protocol redesign

--- src/jiraph/layer.clj
+++ src/jiraph/layer.clj
@@ -1,20 +1,24 @@
 (ns jiraph.layer
   (use [retro.core :only [*revision*]]))
 
-(defn make-node [attrs]
-  (let [attrs (dissoc attrs :id)]
-    (if *revision*
-      (assoc attrs :rev *revision*)
-      (dissoc attrs :rev))))
+(defprotocol Counted
+  (node-count       [layer]            "Return the total number of nodes in this layer."))
+
+(defprotocol Schema
+  (fields
+    [layer]
+    [layer subfields]
+    "A list of canonical fields stored in this layer. Can be empty.")
+  (node-valid?  [layer attrs]      "Check if the given node is valid according to the layer schema."))
 
 (defprotocol LayerMeta
-  (node-count       [layer]            "Return the total number of nodes in this layer.")
-  (node-ids         [layer]            "Return a lazy sequence of all node ids in this layer.")
-  (fields           [layer]
-                    [layer subfields]  "A list of canonical fields stored in this layer. Can be empty.")
-  (node-valid?      [layer attrs]      "Check if the given node is valid according to the layer schema.")
-  (get-property     [layer key]        "Fetch a layer-wide property.")
-  (set-property!    [layer key val]    "Store a layer-wide property."))
+  (get-meta         [layer key]        "Fetch a layer-wide property.")
+  (assoc-meta!      [layer key val]    "Store a layer-wide property."))
+
+(defprotocol Revisioned
+  (get-revision
+    [layer] [layer id]
+    "Return the revision this layer is at, or the revision of the specified node."))
 
 (defprotocol Incoming
   (get-incoming     [layer id]         "Return the ids of all nodes that have an incoming edge to this one.")
@@ -23,10 +27,8 @@
 
 (defprotocol Compound
   (get-node         [layer id]         "Fetch a node.")
-  (set-node!        [layer id attrs]   "Set a node.")
-  (add-node!        [layer id attrs]   "Add a node with the given id and attrs if it doesn't already exist.")
-  (update-node!     [layer id f args]  "Update a node with (apply f node args), and return [old new].")
-  (delete-node!     [layer id]         "Remove a node from the layer (incoming links remain)."))
+  (assoc-node!      [layer id attrs]   "Add a node to the database.")
+  (dissoc-node!     [layer id]         "Remove a node from the database."))
 
 (defprotocol Layer
   "Jiraph layer protocol"
@@ -38,22 +40,29 @@
   (get-revisions    [layer id]         "Return all revision ids for a given node.")
   (node-exists?     [layer id]         "Check if a node exists on this layer."))
 
+(defprotocol Seqable
+  (seq-in-node [layer keyseq] "A seq of all the properties under the given keyseq."))
+
 (defprotocol Sorted
   "Methods for sorted layers."
-  (edges-subseq  [layer id test key] [layer id stest skey etest ekey])
-  (edges-rsubseq [layer id test key] [layer id stest skey etest ekey])
-  (delete-edges-subseq! [layer id test key] [layer id test key etest ekey]))
+  (seq-in-node
+    [layer keyseq test key]
+    [layer keyseq stest skey etest ekey])
+  (rseq-in-node
+    [layer keyseq test key]
+    [layer keyseq stest skey etest ekey])
+  (dissoc-subseq-in-node
+    [layer keyseq test key]
+    [layer keyseq test key etest ekey]))
 
-(defprotocol Split
-  "Layer that can retrieve and deserialize node attributes separately from its edges"
-  (get-attrs [layer id] "Read a node's attributes")
-  (get-edges [layer id] "Read all a node's edges")
-  (get-named-edges [layer id to-ids] "Get specified edges, if they exist"))
+(defprotocol Nested
+  "Layer that has indexed access into some subset of 'deeper' attributes, such as edges."
+  (get-in-node [layer id keyseq])
+  (assoc-in-node! [layer id keyseq value])
+  (dissoc-in-node! [layer id keyseq]))
 
-(defprotocol Append
-  "Jiraph appending format"
-  (append-node! [layer id attrs] "Append attrs to a node or create it if it doesn't exist."))
-
-(defprotocol Assoc
-  "Jiraph assoc protocol"
-  (assoc-node! [layer id attrs] "Associate attrs with a node."))
+(defprotocol Update
+  "Layer that can do (some) updates of sub-nodes more efficiently than fetching a whole node, changing it, and then writing it."
+  (update-in-node!
+    [layer keyseq f argseq]
+    "Arguments are as to clojure.core/update-in, but without varargs."))
