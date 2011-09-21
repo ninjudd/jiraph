d4dd5465e49f69afc258e6316fd2e33a97ed7fd2
Alan Malloy <alan@malloys.org>
2011-09-21 11:33:29 -0700

    More WIP on default impls

--- src/jiraph/layer.clj
+++ src/jiraph/layer.clj
@@ -13,54 +13,9 @@
        (fallback-warning ~'layer '~impl))
      ~impl))
 
-(let [layer-meta-key (fn [id] (str "_" id))
-      node-meta-key (fn [node key] (str "_" node "_" key))
-      incoming-key (fn [id] (node-meta-key id "incoming"))]
-
-  (extend-type Object
-    Schema
-    (fields
-      ([layer] nil)
-      ([layer subfields] nil))
-    ;; TODO add a whats-wrong-with-this-node function
-    (node-valid? [layer attrs] true)
-
-    LayerMeta
-    ;; Implemented in terms of Compound
-    (get-layer-meta [layer key]
-      ((fallback get-node) layer (layer-meta-key key)))
-    (assoc-layer-meta! [layer key value]
-      ((fallback assoc-node!) layer (layer-meta-key key) value))
-
-    NodeMeta
-    ;; Implemented in terms of LayerMeta
-    (get-meta [layer node key]
-      ((fallback get-layer-meta) layer (node-meta-key node key)))
-    (assoc-meta! [layer node key val]
-      ((fallback assoc-layer-meta!) layer (node-meta-key node key) val))
-
-    UpdateMeta
-    (update-meta! [layer node key f args]
-      ((fallback assoc-meta!) layer node key (apply f (get-meta layer node key) args)))
-
-    Counted
-    ;; sorry, you have to implement this yourself
-
-    Incoming
-    (get-incoming [layer id]
-      ((fallback get-meta) layer id (incoming-key id)))
-    (add-incoming! [layer id from-id]
-      ((fallback update-meta!) layer id (incoming-key id) conj from-id))
-    (drop-incoming! [layer id from-id]
-      ((fallback update-meta!) layer id (incoming-key id) disj from-id))
-
-    Compound
-    (get-node [layer id]
-      ((fallback get-in-node) [layer id []]))
-    (assoc-node! [layer id attrs]
-      ((fallback assoc-in-node!) layer id [] attrs))
-    (dissoc-node! [layer id]
-      ((fallback dissoc-in-node!) layer id []))))
+(defprotocol Enumerate
+  (node-id-seq [layer] "A seq of all node ids in this layer")
+  (node-seq    [layer] "A seq of all nodes in this layer"))
 
 (defprotocol Counted
   (node-count       [layer]            "Return the total number of nodes in this layer."))
@@ -79,14 +34,9 @@
 (defprotocol NodeMeta
   (get-meta               [layer node key])
   (assoc-meta!            [layer node key val]))
+
 (defprotocol UpdateMeta
   (update-meta!           [layer node key f args]))
-
-(defprotocol Revisioned
-  (get-revision
-    [layer] [layer id]
-    "Return the revision this layer is at, or the revision of the specified node."))
-
 (defprotocol Incoming
   (get-incoming     [layer id]         "Return the ids of all nodes that have an incoming edge to this one.")
   (add-incoming!    [layer id from-id] "Add an incoming edge record on id for from-id.")
@@ -96,6 +46,11 @@
   (get-node         [layer id]         "Fetch a node.")
   (assoc-node!      [layer id attrs]   "Add a node to the database.")
   (dissoc-node!     [layer id]         "Remove a node from the database."))
+
+(defprotocol Revisioned
+  (get-revision
+    [layer] [layer id]
+    "Return the revision this layer is at, or the revision of the specified node."))
 
 (defprotocol Layer
   "Jiraph layer protocol"
@@ -109,6 +64,101 @@
 
 (defprotocol Seqable
   (seq-in-node [layer keyseq] "A seq of all the properties under the given keyseq."))
+
+(let [layer-meta-key (fn [id] (str "_" id))
+      node-meta-key (fn [node key] (str "_" node "_" key))
+      incoming-key (fn [id] (node-meta-key id "incoming"))]
+
+  (extend-type Object
+    Schema
+    ;; default behavior: no fields, all nodes valid
+    (fields
+      ([layer] nil)
+      ([layer subfields] nil))
+    ;; TODO add a whats-wrong-with-this-node function
+    (node-valid? [layer attrs] true)
+
+    LayerMeta
+    ;; default behavior: create specially-named regular nodes to hold metadata
+    (get-layer-meta [layer key]
+      ((fallback get-node) layer (layer-meta-key key)))
+    (assoc-layer-meta! [layer key value]
+      ((fallback assoc-node!) layer (layer-meta-key key) value))
+
+    NodeMeta
+    ;; default behavior: use specially-named layer-wide meta to fake node metadata
+    (get-meta [layer node key]
+      ((fallback get-layer-meta) layer (node-meta-key node key)))
+    (assoc-meta! [layer node key val]
+      ((fallback assoc-layer-meta!) layer (node-meta-key node key) val))
+
+    UpdateMeta
+    ;; default behavior: get old meta, apply f, set new value
+    (update-meta! [layer node key f args]
+      ((fallback assoc-meta!) layer node key (apply f ((fallback get-meta) layer node key) args)))
+
+    Enumerate
+    ;; Fallback behavior is the empty list. Jiraph does *not* automatically
+    ;; track assoc'd and dissoc'd nodes for you in order to provide node-ids,
+    ;; because that would be a huge performance hit.
+    (node-id-seq [layer]
+      ())
+    (node-seq [layer]
+      ())
+
+    Counted
+    ;; default behavior: walk through all node ids, counting. not very fast
+    (node-count [layer]
+      (apply + (map (constantly 1) ((fallback node-ids) layer))))
+
+    Incoming
+    ;; default behavior: use node meta with special prefix to track incoming edges
+    (get-incoming [layer id]
+      ((fallback get-meta) layer id (incoming-key id)))
+    (add-incoming! [layer id from-id]
+      ((fallback update-meta!) layer id (incoming-key id) conj from-id))
+    (drop-incoming! [layer id from-id]
+      ((fallback update-meta!) layer id (incoming-key id) disj from-id))
+
+    Compound
+    ;;
+    (get-node [layer id]
+      ((fallback get-in-node) [layer id []]))
+    (assoc-node! [layer id attrs]
+      ((fallback assoc-in-node!) layer id [] attrs))
+    (dissoc-node! [layer id]
+      ((fallback dissoc-in-node!) layer id []))
+
+    Revisioned
+    (get-revision
+      ([layer] 0)
+      ([layer id] 0))
+
+    Layer
+    ;; default implementation is to not do anything, hoping you do it
+    ;; automatically at reasonable times, or don't need it done at all
+    (open [layer] nil)
+    (close [layer] nil)
+    (sync! [layer] nil)
+    (optimize! [layer] nil)
+    (get-revisions [layer id]
+      ())
+
+    ;; we can simulate these for you, pretty inefficiently
+    (truncate! [layer]
+      (let [dissoc! (fallback dissoc-node!)]
+        (doseq [id ((fallback node-ids) layer)]
+          (dissoc! layer id))))
+    (node-exists? [layer id]
+      (boolean ((fallback get-node) layer id)))
+
+    Seqable
+    (seq-in-node [layer keyseq]
+      (seq ((fallback get-in-node) layer keyseq)))
+
+    Sorted
+    (seq-in-node [layer keyseq test key]
+      )))
 
 (defprotocol Sorted
   "Methods for sorted layers."
