4c49b9f16ebbf49d470b67de27c02ec9e8d42799
Alan Malloy <alan@malloys.org>
2011-09-28 15:13:12 -0700

    Introduce as-layer-map to unify some things.
    The code to "magically" coerce various input types to layers
    was being repeated in a few places.

--- src/jiraph/core.clj
+++ src/jiraph/core.clj
@@ -14,18 +14,24 @@
         (throw (java.io.IOException. (format "cannot find layer %s in open graph" layer-name))))
     (throw (java.io.IOException. (format "attempt to use a layer without an open graph")))))
 
+(defn as-layer-map
+  "Create a map of {layer-name, layer} pairs from the input. A keyword yields a
+   single-entry map, an empty sequence operates on all layers, and a sequence of
+   keywords causes each to be resolved to its actual layer."
+  [layers]
+  (cond (keyword? layers) {layers (layer layers)}
+        (empty? layers)   *graph*
+        :else             (select-keys *graph* layers)))
+
 (defmacro with-each-layer
   "Execute forms with layer bound to each layer specified or all layers if layers is empty."
   [layers & forms]
-  `(let [layers# ~layers]
-     (doseq [[~'layer-name ~'layer] (cond (keyword? layers#) [layers# (layer layers#)]
-                                          (empty?   layers#) *graph*
-                                          :else              (select-keys *graph* layers#))]
-       (when *verbose*
-         (println (format "%-20s %s"
-                          ~'layer-name
-                          (s/join " " (map pr-str '~forms)))))
-       ~@forms)))
+  `(doseq [[~'layer-name ~'layer] (as-layer-map ~layers)]
+     (when *verbose*
+       (println (format "%-20s %s"
+                        ~'layer-name
+                        (s/join " " (map pr-str '~forms)))))
+     ~@forms))
 
 (defn open! []
   (with-each-layer []
@@ -55,11 +61,5 @@
 (defmacro with-transaction
   "Execute forms within a transaction on the named layer/layers."
   [layers & forms]
-  `(if (read-only?)
-     (do ~@forms)
-     ((reduce
-       retro/wrap-transaction
-       (fn [] ~@forms)
-       (cond (keyword? ~layers) [(layer ~layers)]
-             (empty?   ~layers) (vals *graph*)
-             :else              (map layer ~layers))))))
+  `(graph/with-transaction (vals (as-layer-map ~layers))
+     ~@forms))
