5596b7166af653d4d2ccec7dd60341c7899af61b
Justin Balthrop <git@justinbalthrop.com>
2011-10-05 18:23:26 -0700

    doc formatting tweaks

--- src/jiraph/layer.clj
+++ src/jiraph/layer.clj
@@ -14,96 +14,120 @@
        (fallback-warning ~'layer '~impl))
      ~impl))
 
-(defprotocol Counted
-  (node-count       [layer]            "Return the total number of nodes in this layer."))
-
 (defprotocol Enumerate
-  (node-id-seq [layer] "A seq of all node ids in this layer")
-  (node-seq    [layer] "A seq of all nodes in this layer"))
+  (node-id-seq [layer]
+    "A seq of all node ids in this layer")
+  (node-seq [layer]
+    "A seq of all nodes in this layer"))
+
+(defprotocol Counted
+  (node-count [layer]
+    "Return the total number of nodes in this layer."))
 
 (defprotocol Schema
-  (fields
-    [layer]
-    [layer subfields]
+  (fields [layer] [layer subfields]
     "A list of canonical fields stored in this layer. Can be empty.")
-  (node-valid?  [layer attrs]      "Check if the given node is valid according to the layer schema."))
+  (node-valid? [layer attrs]
+    "Check if the given node is valid according to the layer schema."))
 
 (defprotocol LayerMeta
-  (get-layer-meta         [layer key]        "Fetch a layer-wide property.")
-  (assoc-layer-meta!      [layer key val]    "Store a layer-wide property."))
+  (get-layer-meta [layer key]
+    "Fetch layer-wide meta.")
+  (assoc-layer-meta! [layer key val]
+    "Store layer-wide meta."))
 
 (defprotocol NodeMeta
-  (get-meta               [layer node key])
-  (assoc-meta!            [layer node key val]))
+  (get-meta [layer node key]
+    "Fetch the metadata for a specific node.")
+  (assoc-meta! [layer node key val]
+    "Set the metadata for a node."))
 
 (defprotocol UpdateMeta
-  (update-meta!           [layer node key f args]))
+  (update-meta! [layer node key f args]
+    "Update the metadata for a node."))
 
 (defprotocol Incoming
-  (get-incoming     [layer id]         "Return the ids of all nodes that have an incoming edge to this one.")
-  (add-incoming!    [layer id from-id] "Add an incoming edge record on id for from-id.")
-  (drop-incoming!   [layer id from-id] "Remove the incoming edge record on id for from-id."))
+  (get-incoming [layer id]
+    "Return the ids of all nodes that have an incoming edge to this one.")
+  (add-incoming! [layer id from-id]
+    "Add an incoming edge record on id for from-id.")
+  (drop-incoming! [layer id from-id]
+    "Remove the incoming edge record on id for from-id."))
 
 (defprotocol Basic
-  (get-node      [layer id not-found] "Fetch a node.")
-  (assoc-node!   [layer id attrs]     "Add a node to the database.")
-  (dissoc-node!  [layer id]           "Remove a node from the database."))
+  (get-node [layer id not-found]
+    "Fetch a node from the graph.")
+  (assoc-node! [layer id attrs]
+    "Add a node to the graph.")
+  (dissoc-node! [layer id]
+    "Remove a node from the graph."))
 
 (defprotocol Optimized
-  "Describe to jiraph how to perform a more-optimized version of some subset of operations on a
+  "Describe to Jiraph how to perform a more-optimized version of some subset of operations on a
   layer. These functions will be called on a layer in preparation to actually performing an
   operation on some internal node. The layer is responsible for determining whether that operation
   can be optimized at that position in the tree; this will usually be implemented by inspecting
   the function passed in and testing it against some internal list of optimizable functions.
 
   If nil is returned, the operation will be done in some less optimized way, often by calling
-  get-node, applying the update, and then assoc-node!.  If an optimization is possible, you should
+  get-node, applying the update, and then assoc-node!. If an optimization is possible, you should
   return a function which will take any additional args to f (but not f itself, or the keyseq;
-  those should be closed over and captured in the returned function); jiraph will call this
+  those should be closed over and captured in the returned function); Jiraph will call this
   function with the appropriate arguments.
 
-  For example, jiraph might ask for an optimization with (update-fn layer [from-id :edges to-id]
+  For example, Jiraph might ask for an optimization with (update-fn layer [from-id :edges to-id]
   assoc), and call the returned function as (the-fn :rel :child), thus achieving basically the
   same effect as (update-in layer [from-id :edges to-id] assoc :rel :child), or (assoc-in layer
   [from-id :edges to-id :rel] child)."
 
-  (update-fn [layer keyseq f] "Get a function for performing an optimized update/mutation on the
-  layer at a specified node. See documentation of Optimized for the general contract of Optimized
-  functions. In addition, the function returned by update-fn should, when called, return a hash
-  containing information about the changes it made, if possible.
+  (update-fn [layer keyseq f]
+    "Get a function for performing an optimized update/mutation on the layer at a specified
+    node. See documentation of Optimized for the general contract of Optimized functions. In
+    addition, the function returned by update-fn should, when called, return a hash containing
+    information about the changes it made, if possible.
 
-  The hash should contain keys :old and :new, describing the contents of the sub-node (that is,
-  the one in the layer under keyseq) before and after the update was applied. It is legal for
-  these to both be nil (eg, returning an empty hash), but in that case jiraph will be unable to
-  provide some services, such as managing incoming edges for you.
+    The hash should contain keys :old and :new, describing the contents of the sub-node (that is,
+    the one in the layer under keyseq) before and after the update was applied. It is legal for
+    these to both be nil (eg, returning an empty hash), but in that case Jiraph will be unable to
+    provide some services, such as managing incoming edges for you.
 
-  jiraph's behavior in the case of return values of any type other than hash (including nil) is
-  unspecified; these may receive special handling in some future version.")
+    Jiraph's behavior in the case of return values of any type other than hash (including nil) is
+    unspecified; these may receive special handling in some future version.")
 
-  (query-fn [layer keyseq f] "Get a function for performing an optimized read on the layer at a
-  specified node. See documentation of Optimized for the general contract of Optimized
-  functions. The function returned by update-fn should, when called, call f on the data at
-  keyseq (presumably in some optimized way), and return f's result.
+  (query-fn [layer keyseq f]
+    "Get a function for performing an optimized read on the layer at a specified node. See
+    documentation of Optimized for the general contract of Optimized functions. The function
+    returned by query-fn should, when called, call f on the data at keyseq (presumably in some
+    optimized way), and return f's result.
 
-  For example, a layer might store a node's edge-count in a separate field which can be read
-  without reading the edges themselves; in that case, (query-fn layer [node-name :edges] count)
-  should return a function like (fn [] (get-from-db (str node-name \":edge-count\")))."))
+    For example, a layer might store a node's edge-count in a separate field which can be read
+    without reading the edges themselves; in that case, (query-fn layer [node-name :edges] count)
+    should return a function like (fn [] (get-from-db (str node-name \":edge-count\")))."))
 
 (defprotocol Layer
   "Jiraph layer protocol"
-  (open             [layer]            "Open the layer file.")
-  (close            [layer]            "Close the layer file.")
-  (sync!            [layer]            "Flush all layer changes to the storage medium.")
-  (optimize!        [layer]            "Optimize underlying layer storage.")
-  (truncate!        [layer]            "Removes all node data from the layer.")
-  (get-revisions    [layer id]         "Return all revision ids for a given node.")
-  (node-exists?     [layer id]         "Check if a node exists on this layer."))
+  (open [layer]
+    "Open the layer file.")
+  (close [layer]
+    "Close the layer file.")
+  (sync! [layer]
+    "Flush all layer changes to the storage medium.")
+  (optimize! [layer]
+    "Optimize underlying layer storage.")
+  (truncate! [layer]
+    "Removes all node data from the layer.")
+  (get-revisions [layer id]
+    "Return all revision ids for a given node.")
+  (node-exists? [layer id]
+    "Check if a node exists on this layer."))
 
 (defprotocol Preferences
-  "Indicate to jiraph what things you want it to do for you. These preferences should not
-  change while the system is running; jiraph may choose to cache any of them."
-  (manage-incoming? [layer] "Should jiraph decide when to add/drop incoming edges?")
-  (single-edge?     [layer] "Is it illegal to have more than one edge from a node on this layer?"))
+  "Indicate to Jiraph what things you want it to do for you. These preferences should not
+  change while the system is running; Jiraph may choose to cache any of them."
+  (manage-incoming? [layer]
+    "Should Jiraph decide when to add/drop incoming edges?")
+  (single-edge? [layer]
+    "Is it illegal to have more than one outgoing edge per node on this layer?"))
 
 ;; Meta-related protocols
 (letfn [(layer-meta-key [id] (str "_" id))
@@ -144,9 +168,9 @@
     Layer
     ;; default implementation is to not do anything, hoping you do it
     ;; automatically at reasonable times, or don't need it done at all
-    (open [layer] nil)
-    (close [layer] nil)
-    (sync! [layer] nil)
+    (open      [layer] nil)
+    (close     [layer] nil)
+    (sync!     [layer] nil)
     (optimize! [layer] nil)
 
     ;; we can simulate these for you, pretty inefficiently
@@ -154,6 +178,7 @@
       (let [dissoc! (fallback dissoc-node!)]
         (doseq [id ((fallback node-id-seq) layer)]
           (dissoc! layer id))))
+
     (node-exists? [layer id]
       (not= sentinel ((fallback get-node) layer id sentinel)))
 
@@ -161,7 +186,9 @@
     ;; opt in to managed-incoming, and let the layer set a :single-edge key to
     ;; indicate it should be in single-edge mode
     (manage-incoming? [layer] true)
-    (single-edge? [layer] (:single-edge layer))))
+
+    (single-edge? [layer]
+      (:single-edge layer))))
 
 ;; Don't need any special closures here
 (extend-type Object
@@ -170,6 +197,7 @@
   (fields
     ([layer] nil)
     ([layer subfields] nil))
+
   ;; TODO add a whats-wrong-with-this-node function
   (node-valid? [layer attrs] true)
 
@@ -179,6 +207,7 @@
   ;; because that would be a huge performance hit.
   (node-id-seq [layer]
     ())
+
   (node-seq [layer]
     ())
 
@@ -190,7 +219,7 @@
   Optimized
   ;; can't optimize anything
   (update-fn [layer keyseq f] nil)
-  (query-fn [layer keyseq f] nil))
+  (query-fn  [layer keyseq f] nil))
 
 (defn default-impl [protocol]
   (get-in protocol [:impls Object]))
