3aacc31368511320b1554ada0dc919953436e2ea
Justin Balthrop <git@justinbalthrop.com>
2011-10-10 17:57:40 -0700

    update deps

--- project.clj
+++ project.clj
@@ -1,13 +1,11 @@
-(defproject jiraph "0.7.0-beta5"
+(defproject jiraph "0.7.0-beta3"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.7.0-beta5"]
+                 [useful "0.7.0"]
                  [masai "0.5.3"]
-                 [cereal "0.1.5"]
-                 [retro "0.6.0-alpha1"]
+                 [cereal "0.1.8"]
+                 [retro "0.5.2"]
                  [ego "0.1.5"]]
-  :dev-dependencies [[protobuf "0.5.0-alpha4"]
-                     ;; [org.clojars.flatland/cake-marginalia "0.6.3"]
+  :dev-dependencies [[protobuf "0.5.0-beta1"]
                      [tokyocabinet "1.24.1-SNAPSHOT" :ext true]]
-  ;; :tasks [protobuf.tasks cake-marginalia.tasks]
-  :cake-plugins [[cake-protobuf "0.5.0-alpha5"]])
+  :cake-plugins [[cake-protobuf "0.5.0-beta1"]])
