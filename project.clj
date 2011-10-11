adb50cee727eb00425ee6a129b02307c43365436
Justin Balthrop <git@justinbalthrop.com>
2011-10-11 13:30:51 -0700

    update masai and ego deps

--- project.clj
+++ project.clj
@@ -1,11 +1,11 @@
-(defproject jiraph "0.7.0-beta6"
+(defproject jiraph "0.7.0-beta7"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.2.0"]
                  [useful "0.7.0"]
-                 [masai "0.5.3"]
+                 [masai "0.6.0"]
                  [cereal "0.1.9"]
                  [retro "0.5.2"]
-                 [ego "0.1.5"]]
+                 [ego "0.1.7"]]
   :dev-dependencies [[protobuf "0.5.0-beta1"]
                      [tokyocabinet "1.24.1-SNAPSHOT" :ext true]]
   :cake-plugins [[cake-protobuf "0.5.0-beta1"]])
