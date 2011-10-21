01ffe3e3d0ff4ac3d85556aef4b7f94337d609e9
lance bradley <l@ncebradley.org>
2011-10-21 16:57:51 -0700

    add typehints to jiraph.masai-layer

--- src/jiraph/masai_layer.clj
+++ src/jiraph/masai_layer.clj
@@ -23,65 +23,7 @@
 (defn- property-key [key]
   (str property-prefix key))
 
-(declare meta-len)
-(defn- get-meta [layer key rev]
-  (let [db (.db layer)
-        mf (.meta-format layer)]
-    (if rev
-      (let [len (meta-len layer key rev)]
-        (when (< 0 len)
-          (f/decode mf (db/get db key) 0 len)))
-      (f/decode mf (db/get db key)))))
-
-(defn- len
-  "The byte length of the node at revision rev."
-  [layer id rev]
-  (if-not rev
-    (db/len (.db layer) id)
-    (let [meta (get-meta layer (meta-key id) nil)
-          len  (find-with (partial >= rev)
-                          (reverse (:rev meta))
-                          (reverse (:len meta)))]
-      (when (and len (<= 0 len))
-        len))))
-
-(defn- meta-len
-  "The byte length of the meta node at revision rev."
-  [layer key rev]
-  (or (when rev
-        (let [meta (get-meta layer key nil)]
-          ;; Must shift meta lengths by one since they store the length of the previous revision.
-          (find-with (partial >= rev)
-                     (reverse (cons 0 (:mrev meta)))
-                     (cons nil (reverse (:mlen meta))))))
-      (db/len (.db layer) key)))
-
-(defn- append-meta! [layer id attrs & [rev]]
-  (let [db (.db layer)
-        mf (.meta-format layer)
-        key (meta-key id)]
-    (->> (if rev
-           (assoc attrs :mrev rev :mlen (db/len db key))
-           attrs)
-         (f/encode mf)
-         (db/append! db key))))
-
-(defn- set-len! [layer id len]
-  (if *revision*
-    (append-meta! layer id {:rev *revision* :len len})))
-
-(defn- reset-len! [layer id & [len]]
-  ;; Insert a length of -1 to indicate that all lengths before the current one are no longer valid.
-  (append-meta! layer id
-    (if (and *revision* len)
-      {:rev [0 *revision*] :len [-1 len]}
-      {:rev 0 :len -1})))
-
-(defn- inc-count! [layer]
-  (db/inc! (.db layer) count-key 1))
-
-(defn- dec-count! [layer]
-  (db/inc! (:db layer) count-key -1))
+(declare meta-len get-meta len meta-len append-meta! set-len! reset-len! inc-count! dec-count!)
 
 (defrecord MasaiLayer [db format meta-format]
   jiraph.layer/Layer
@@ -95,7 +37,7 @@
     (db/inc! db count-key 0))
 
   (node-ids [layer]
-    (remove #(.startsWith % meta-prefix) (db/key-seq db)))
+    (remove #(.startsWith ^String % meta-prefix) (db/key-seq db)))
 
   (fields [layer]
     (f/fields format))
@@ -109,7 +51,7 @@
          (catch Exception e)))
 
   (get-property  [layer key]
-    (if-let [bytes (db/get db (property-key key))]
+    (if-let [bytes ^bytes (db/get db (property-key key))]
       (read-string (String. bytes))))
 
   (set-property! [layer key val]
@@ -128,14 +70,14 @@
 
   (add-node! [layer id attrs]
     (let [node (make-node attrs)
-          data (f/encode format node)]
+          data ^bytes (f/encode format node)]
       (when (db/add! db id data)
         (inc-count! layer)
         (set-len! layer id (alength data))
         (f/decode format data))))
 
   (set-node! [layer id attrs]
-    (let [data (f/encode format (make-node attrs))]
+    (let [data ^bytes (f/encode format (make-node attrs))]
       (db/put! db id data)
       (reset-len! layer id (alength data))
       (f/decode format data)))
@@ -161,7 +103,7 @@
     (when-not (empty? attrs)
       (let [len  (db/len db id)
             node (make-node attrs)
-            data (f/encode format node)]
+            data ^bytes (f/encode format node)]
         (db/append! db id data)
         (when (= -1 len)
           (inc-count! layer))
@@ -205,3 +147,62 @@
          (if (instance? cereal.reader.ReaderFormat format)
            (reader-append-format/make {:in #{} :rev [] :len [] :mrev [] :mlen []})
            (make-protobuf format))))))
+
+(defn- get-meta [^MasaiLayer layer key rev]
+  (let [db (.db layer)
+        mf (.meta-format layer)]
+    (if rev
+      (let [len (meta-len layer key rev)]
+        (when (< 0 len)
+          (f/decode mf (db/get db key) 0 len)))
+      (f/decode mf (db/get db key)))))
+
+(defn- len
+  "The byte length of the node at revision rev."
+  [^MasaiLayer layer id rev]
+  (if-not rev
+    (db/len (.db layer) id)
+    (let [meta (get-meta layer (meta-key id) nil)
+          len  (find-with (partial >= rev)
+                          (reverse (:rev meta))
+                          (reverse (:len meta)))]
+      (when (and len (<= 0 len))
+        len))))
+
+(defn- meta-len
+  "The byte length of the meta node at revision rev."
+  [^MasaiLayer layer key rev]
+  (or (when rev
+        (let [meta (get-meta layer key nil)]
+          ;; Must shift meta lengths by one since they store the length of the previous revision.
+          (find-with (partial >= rev)
+                     (reverse (cons 0 (:mrev meta)))
+                     (cons nil (reverse (:mlen meta))))))
+      (db/len (.db layer) key)))
+
+(defn- append-meta! [^MasaiLayer layer id attrs & [rev]]
+  (let [db (.db layer)
+        mf (.meta-format layer)
+        key (meta-key id)]
+    (->> (if rev
+           (assoc attrs :mrev rev :mlen (db/len db key))
+           attrs)
+         (f/encode mf)
+         (db/append! db key))))
+
+(defn- set-len! [^MasaiLayer layer id len]
+  (if *revision*
+    (append-meta! layer id {:rev *revision* :len len})))
+
+(defn- reset-len! [^MasaiLayer layer id & [len]]
+  ;; Insert a length of -1 to indicate that all lengths before the current one are no longer valid.
+  (append-meta! layer id
+    (if (and *revision* len)
+      {:rev [0 *revision*] :len [-1 len]}
+      {:rev 0 :len -1})))
+
+(defn- inc-count! [^MasaiLayer layer]
+  (db/inc! (.db layer) count-key 1))
+
+(defn- dec-count! [^MasaiLayer layer]
+  (db/inc! (:db layer) count-key -1))
