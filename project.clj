b677386dc5d050236ea5759ceb84f2d31480b71d
lance bradley <l@ncebradley.org>
2011-10-21 16:21:01 -0700

    v 0.7.0-beta9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jiraph "0.7.0-beta8"
+(defproject jiraph "0.7.0-beta9"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.2.0"]
                  [useful "0.7.0"]
@@ -6,6 +6,6 @@
                  [cereal "0.1.10"]
                  [retro "0.5.2"]
                  [ego "0.1.7"]]
-  :dev-dependencies [[protobuf "0.5.0-beta1"]
+  :dev-dependencies [[protobuf "0.5.0-beta2"]
                      [tokyocabinet "1.24.1-SNAPSHOT" :ext true]]
   :cake-plugins [[cake-protobuf "0.5.0-beta1"]])
