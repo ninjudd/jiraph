d112f112753f47ad0c70ca32f99ac2cbff87743d
Justin Balthrop <git@justinbalthrop.com>
2011-10-31 17:38:33 -0700

    formatting

--- src/jiraph/walk/predicates.clj
+++ src/jiraph/walk/predicates.clj
@@ -3,20 +3,21 @@
 
 (defn at-limit
   "Returns a function that can be passed as the :terminate? traversal parameter
-   to limit a walk to a specific number of steps."
+  to limit a walk to a specific number of steps."
   [num]
   (fn [walk]
     (<= num (result-count walk))))
 
 (defn has-distance
-  "Return a predicate taking [walk step], which applies the supplied test to the
-   step's distance. The distance of a step is the distance from the walk's
-   initial node to the step's destination node."
+  "Return a predicate taking [walk step], which applies the supplied test to the step's
+  distance. The distance of a step is the distance from the walk's initial node to the step's
+  destination node."
   [test num]
   (fn [walk step]
     (test (distance step) num)))
 
 (defn initial-step?
-  "Is this the first step of the walk? Two arity verson can be used for :traverse?, :add? and :follow?."
+  "Is this the first step of the walk? Two arity verson can be used for :traverse?, :add?
+  and :follow?."
   ([_ step] (initial-step? step))
   ([step]   (nil? (from-id step))))
