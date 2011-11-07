03e28e70ed317dfc8cb147dac1c65401db626101
Alan Malloy <alan@malloys.org>
2011-11-07 15:42:14 -0800

    Add test to STM layer's query-fn

--- test/jiraph/stm_test.clj
+++ test/jiraph/stm_test.clj
@@ -14,3 +14,13 @@
            (get-node (doto (make filename) layer/open)
                      "test")))
     (-> filename java.io.File. .delete)))
+
+(deftest queryfn-test
+  (let [master (make)
+        update-counter (atom 0)]
+    (dotxn master
+      (-> master
+          (assoc-node "mikey" {:height {:feet 5 :inches 4}
+                               :edges {"jennifer" {:rel :child}}})))
+    (is (= 2 (query-in-node master ["mikey" :height] 'specialized-count update-counter)))
+    (is (= 1 @update-counter))))
