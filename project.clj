04bbc163c961792cf26d2065d94a3ec51231d077
Anthony Grimes <i@raynes.me>
2011-12-09 16:10:29 -0600

    Update to 1.3.

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
 (defproject jiraph "0.7.0-beta7"
   :description "embedded graph db library for clojure"
-  :dependencies [[clojure "1.2.0"]
+  :dependencies [[clojure "1.3.0"]
                  [useful "0.7.5-alpha2"]
                  [masai "0.7.0-alpha4"]
-                 [cereal "0.2.0-alpha2"]
+                 [cereal "0.2.0-alpha3"]
                  [retro "0.6.0-alpha6"]
                  [io "0.1.0-alpha2"]
                  [ego "0.1.7"]]
