9f7ee8905d110df991de6af62fdd9f00cab151f6
Anthony Grimes <i@raynes.me>
2011-12-09 19:39:52 -0600

    Update more deps.

--- project.clj
+++ project.clj
@@ -1,12 +1,11 @@
 (defproject jiraph "0.7.0-beta7"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.3.0"]
-                 [useful "0.7.5-alpha2"]
-                 [masai "0.7.0-alpha4"]
+                 [useful "0.7.5-alpha4"]
+                 [masai "0.7.0-alpha5"]
                  [cereal "0.2.0-alpha3"]
                  [retro "0.6.0-alpha6"]
                  [io "0.1.0-alpha2"]
                  [ego "0.1.7"]]
-  :dev-dependencies [[protobuf "0.5.0-beta1"]
-                     [tokyocabinet "1.24.1-SNAPSHOT" :ext true]]
-  :cake-plugins [[cake-protobuf "0.5.0-beta1"]])
+  :dev-dependencies [[protobuf "0.5.0-beta5"]
+                     [tokyocabinet "1.24.1-SNAPSHOT"]])
