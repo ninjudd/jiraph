40a14609443a30360ba36541ccc658e3e751e762
Justin Balthrop <git@justinbalthrop.com>
2011-12-12 16:12:32 -0800

    stable release

--- project.clj
+++ project.clj
@@ -1,12 +1,12 @@
-(defproject jiraph "0.7.0-beta16"
+(defproject jiraph "0.7.0"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.2.0"]
-                 [useful "0.7.4-alpha4"]
-                 [masai "0.6.1"]
+                 [useful "0.7.5"]
+                 [masai "0.6.2"]
                  [cereal "0.1.10"]
                  [retro "0.5.2"]
                  [ego "0.1.7"]]
-  :dev-dependencies [[protobuf "0.5.0-beta2"]
-                     [tokyocabinet "1.24.1-SNAPSHOT" :ext true]
+  :dev-dependencies [[protobuf "0.5.0"]
+                     [tokyocabinet "1.24.1" :ext true]
                      [unk "0.9.3"]]
-  :cake-plugins [[cake-protobuf "0.5.0-beta1"]])
+  :cake-plugins [[cake-protobuf "0.5.0"]])
