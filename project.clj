c78ff9d4a6fe3dd07e39a9d7aaae6474736540ed
Justin Balthrop <git@justinbalthrop.com>
2011-12-12 18:41:48 -0800

    retro 0.6.0-beta1

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
                  [useful "0.7.5"]
                  [masai "0.7.0-alpha5"]
                  [cereal "0.2.0-alpha3"]
-                 [retro "0.6.0-alpha6"]
+                 [retro "0.6.0-beta1"]
                  [io "0.1.0-alpha2"]
                  [ego "0.1.7"]]
   :dev-dependencies [[protobuf "0.5.0"]
