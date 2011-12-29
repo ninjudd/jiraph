2d6a9e7aed06a7770c856000934c65a50d64b806
Justin Balthrop <git@justinbalthrop.com>
2011-12-28 18:52:40 -0800

    update retro

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
-(defproject jiraph "0.7.1-alpha1"
+(defproject jiraph "0.7.1"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.2.0"]
                  [useful "0.7.5"]
-                 [masai "0.6.2"]
+                 [masai "0.6.3"]
                  [cereal "0.1.10"]
-                 [retro "0.5.2"]
+                 [retro "0.5.3"]
                  [ego "0.1.7"]]
   :dev-dependencies [[protobuf "0.5.0"]
                      [tokyocabinet "1.24.1" :ext true]
