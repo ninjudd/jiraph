4596ec9b440d81ce3fb8dbe6d1d81f816d6d065c
Alan Malloy <alan@malloys.org>
2012-01-31 19:00:20 -0800

    Make protobuf-codec default to using adjoin as its reduce-fn

--- src/jiraph/codecs/protobuf.clj
+++ src/jiraph/codecs/protobuf.clj
@@ -6,14 +6,17 @@
 ;; NB doesn't currently work if you do a full/optimized read with _reset keys.
 ;; plan is to fall back to non-optimized reads in that case, but support an
 ;; option to protobuf-codec to never try an optimized read if you expect _resets
-(defn protobuf-codec [proto reduce-fn]
-  (let [revisioned (-> (protobuf/protobuf-codec proto :repeated true)
-                       (revisioned-codec reduce-fn))]
-    (if (= adjoin reduce-fn)
-      (let [full (protobuf/protobuf-codec proto)]
-        (fn [{:keys [revision]}]
-          (if (nil? revision)
-            full
-            (revisioned revision))))
-      (fn [{:keys [revision]}]
-        (revisioned revision)))))
+(defn protobuf-codec
+  ([proto]
+     (protobuf-codec proto adjoin))
+  ([proto reduce-fn]
+     (let [revisioned (-> (protobuf/protobuf-codec proto :repeated true)
+                          (revisioned-codec reduce-fn))]
+       (if (= adjoin reduce-fn)
+         (let [full (protobuf/protobuf-codec proto)]
+           (fn [{:keys [revision]}]
+             (if (nil? revision)
+               full
+               (revisioned revision))))
+         (fn [{:keys [revision]}]
+           (revisioned revision))))))
