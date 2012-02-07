569d0d2e3dc8a9a51544c9fed579de7f86e8b30d
Alan Malloy <alan@malloys.org>
2012-02-07 12:29:35 -0800

    Bugfix in defwalk - meta should go on function, not var.

--- src/jiraph/walk.clj
+++ src/jiraph/walk.clj
@@ -58,10 +58,12 @@
    In, addition if you wish to cache walk results, use the option :cache true."
   [name & defaults]
   `(let [defaults# (into-map default-traversal ~@defaults)]
-     (defn ~name {:defaults defaults#} [focus-id# & opts#]
-       (let [opts#  (into-map defaults# opts#)
-             cache# (:cache opts#)]
-         ((if cache# cached-walk walk) focus-id# (dissoc opts# :cache))))))
+     (def ~name
+       (-> (fn ~name [focus-id# & opts#]
+             (let [opts#  (into-map defaults# opts#)
+                   cache# (:cache opts#)]
+               ((if cache# cached-walk walk) focus-id# (dissoc opts# :cache))))
+           (with-meta {:defaults defaults#})))))
 
 (defn- walked?
   "Has this step already been traversed?"
