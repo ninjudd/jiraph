263178ca73f5efe115d420c9a9c734292d99ac57
Alan Malloy <alan@malloys.org>
2012-02-21 12:30:06 -0800

    Factor out names of _reset, _rev keys

--- src/jiraph/codecs.clj
+++ src/jiraph/codecs.clj
@@ -4,6 +4,9 @@
             [gloss.core :as gloss]
             [ego.core   :as ego]))
 
+(def reset-key :codec_reset)
+(def revision-key :revisions)
+
 (defn encode [codec val opts] ; (opts -> Codec) -> Deserialized -> opts -> [Byte]
   (io/encode (codec opts) val))
 
@@ -13,33 +16,36 @@
 (defn revisioned-codec [codec-builder reduce-fn] ;; Codec -> (a -> a) -> (opts -> Codec) ;; TODO fix
   ;; TODO take in a map of reduce-fn and (optionally) init-val
   (letfn [(reducer [acc x]
-            (if (:_reset x), x, (reduce-fn acc x)))
+            (if (reset-key x), x, (reduce-fn acc x)))
           (combine [items]
             (when (seq items)
-              (let [node (reduce reducer items)]
-                (with-meta (dissoc node :_rev :_reset)
-                  {:revision (:_rev node)}))))]
+              (let [node (or (reduce reducer items) {})]
+                (with-meta (dissoc node reset-key)
+                  (select-keys node [revision-key])))))]
     (-> (fn [{:keys [revision reset] :as opts}]
           (let [codec (codec-builder opts)
                 maybe-reset (if reset
-                              (fn [data] (assoc data :_reset true))
+                              (fn [data] (assoc data reset-key true))
                               identity)
+                maybe-revision (if revision
+                                 (fn [data] (assoc data revision-key revision))
+                                 identity)
                 frame (fn [pre-encode post-decode]
                         (-> (gloss/compile-frame codec
-                                                 (comp list pre-encode maybe-reset)
+                                                 (comp list pre-encode maybe-reset maybe-revision)
                                                  (comp combine post-decode))
                             (vary-meta assoc :reduce-fn reduce-fn)))]
             (if revision
-              (frame #(assoc % :_rev revision)
+              (frame #(assoc % revision-key revision)
                      (fn [vals]
-                       (take-while #(<= (:_rev %) revision)
+                       (take-while #(<= (revision-key %) revision)
                                    vals)))
               (frame identity identity))))
         (with-meta {:reduce-fn reduce-fn
                     :revisions (fn [opts]
                                  (gloss/compile-frame (codec-builder opts)
                                                       nil ;; never write with this codec
-                                                      (partial map :_rev)))}))))
+                                                      (partial map revision-key)))}))))
 
 (defn wrap-typing [codec-fn types]
   (fn [{:keys [id] :as opts}]
