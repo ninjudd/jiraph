230b374dfb82d084602644e955c453e44f58169f
Alan Malloy <alan@malloys.org>
2012-02-28 18:53:24 -0800

    Fix revisioned-codec to handle reading just revisions

--- src/jiraph/codecs.clj
+++ src/jiraph/codecs.clj
@@ -24,32 +24,36 @@
 (defn revisioned-codec [codec-builder reduce-fn] ;; Codec -> (a -> a) -> (opts -> Codec) ;; TODO fix
   ;; TODO take in a map of reduce-fn and (optionally) init-val
   (letfn [(reducer [acc x]
-            (if (reset-key x), (dissoc x reset-key) (reduce-fn acc x)))
+            (reduce-fn (if (reset-key x)
+                         (select-keys acc [revision-key])
+                         acc)
+                       (dissoc x reset-key)))
           (combine [items]
             (when (seq items)
               (tidy-up (or (reduce reducer items) {}))))]
-    (-> (fn [{reset reset-key, :keys [revision] :as opts}]
-          (let [codec (codec-builder opts)
-                maybe-reset (if reset
-                              (fn [data] (assoc data reset-key true))
-                              identity)
-                frame (fn [pre-encode post-decode]
-                        (-> (gloss/compile-frame codec
-                                                 (comp list pre-encode maybe-reset)
-                                                 (comp combine post-decode))
-                            (vary-meta assoc :reduce-fn reduce-fn)))]
-            (if revision
-              (frame #(assoc % revision-key [revision])
-                     (fn [vals]
-                       ;; this is run BEFORE tidy-up, so revision is not in the meta but in the node
-                       (take-while #(<= (first (revision-key %)) revision)
-                                   vals)))
-              (frame identity identity))))
-        (with-meta {:reduce-fn reduce-fn
-                    :revisions (fn [opts]
-                                 (gloss/compile-frame (codec-builder opts)
-                                                      nil ;; never write with this codec
-                                                      (comp revision-key meta last)))}))))
+    (letfn [(node-codec [{reset reset-key, :keys [revision] :as opts}]
+              (let [codec (codec-builder opts)
+                    maybe-reset (if reset
+                                  (fn [data] (assoc data reset-key true))
+                                  identity)
+                    frame (fn [pre-encode post-decode]
+                            (-> (gloss/compile-frame codec
+                                                     (comp list pre-encode maybe-reset)
+                                                     (comp combine post-decode))
+                                (vary-meta assoc :reduce-fn reduce-fn)))]
+                (if revision
+                  (frame #(assoc % revision-key [revision])
+                         (fn [vals]
+                           ;; run BEFORE tidy-up, so revision is not in the meta but in the node
+                           (take-while #(<= (first (revision-key %)) revision)
+                                       vals)))
+                  (frame identity identity))))]
+      (-> node-codec
+          (with-meta {:reduce-fn reduce-fn
+                      :revisions (fn [opts] ;; read revisions just by discarding the rest
+                                   (gloss/compile-frame (node-codec opts)
+                                                        nil ;; never write with this codec
+                                                        (comp revision-key meta)))})))))
 
 (defn wrap-typing [codec-fn types]
   (fn [{:keys [id] :as opts}]
