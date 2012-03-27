6f6c10aa287e302845af2aae71f510f6e25d6de3
Alan Malloy <alan@malloys.org>
2012-03-27 14:20:57 -0700

    alpha 7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jiraph "0.8.0-alpha6"
+(defproject jiraph "0.8.0-alpha7"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.3.0"]
                  [useful "0.8.0-alpha1"]
@@ -7,7 +7,7 @@
                  [retro "0.6.0-beta1"]
                  [io "0.1.0-alpha2"]
                  [ego "0.1.7"]]
-  :dev-dependencies [[protobuf "0.6.0-beta11"]
+  :dev-dependencies [[protobuf "0.6.0-beta12"]
                      [tokyocabinet "1.24.2" :ext true]
                      [unk "0.9.3"]]
   :checksum-deps true ;; Tired of accidentally running with old deps
