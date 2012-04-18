7823fdd9e4b23f24c709124be0c962d2a4928e1a
Alan Malloy <alan@malloys.org>
2012-04-18 16:19:07 -0700

    alpha 15

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jiraph "0.8.0-alpha14"
+(defproject jiraph "0.8.0-alpha15"
   :description "embedded graph db library for clojure"
   :dependencies [[clojure "1.3.0"]
                  [useful "0.8.0-alpha1"]
