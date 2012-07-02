1ddb5d679f8df8aa00a3b52ed98ee7c387a1b3c0
Anthony Grimes <i@raynes.me>
2012-07-02 17:58:06 -0500

    Move to lein 2.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject jiraph "0.8.0-beta6"
   :description "embedded graph db library for clojure"
-  :dependencies [[clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.4.0"]
                  [useful "0.8.3-alpha6"]
                  [masai "0.7.0-alpha9"]
                  [cereal "0.2.0-alpha3"]
@@ -9,9 +9,10 @@
                  [retro "0.6.0-beta1"]
                  [io "0.2.0-beta2"]
                  [ego "0.1.7"]
-                 [org.clojure/core.match "0.2.0-alpha9"]]
-  :dev-dependencies [[protobuf "0.6.0-beta18"]
-                     [tokyocabinet "1.24.3" :ext true]
-                     [unk "0.9.3"]]
+                 [org.clojure/core.match "0.2.0-alpha9"]
+                 [protobuf "0.6.0-beta19"]]
+  :plugins [[lein-protobuf "0.1.0"]]
+  :profiles {:dev {:dependencies [[tokyocabinet "1.24.3"]
+                                  [unk "0.9.3"]]}}
   :checksum-deps true ;; Tired of accidentally running with old deps
   :hooks [leiningen.protobuf])
