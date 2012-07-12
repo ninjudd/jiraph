359713cb9718daab56dfe9a453827e99e476a94f
Alan Malloy <alan@malloys.org>
2012-07-11 17:50:08 -0700

    Beta9

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jiraph "0.8.0-beta8"
+(defproject jiraph "0.8.0-beta9"
   :description "embedded graph db library for clojure"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [useful "0.8.3-alpha6"]
