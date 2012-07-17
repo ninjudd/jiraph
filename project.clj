75dd87451c098e907e499ea15a545dba44b398aa
Anthony Grimes <i@raynes.me>
2012-07-17 18:58:52 -0500

    project.clj renovations
    * Add :url and :license keys.
    * Add a testall alias task for testing multiple versions of Clojure.
    * Be awesome.

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject jiraph "0.8.0-beta10"
   :description "embedded graph db library for clojure"
+  :url "https://github.com/flatland/jiraph"
+  :license {:name "Eclipse Public License - v 1.0"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [useful "0.8.3-alpha6"]
                  [masai "0.7.0-alpha9"]
@@ -13,7 +16,12 @@
                  [org.clojure/core.match "0.2.0-alpha9"]
                  [protobuf "0.6.0-beta19"]]
   :plugins [[lein-protobuf "0.1.0"]]
-  :profiles {:dev {:dependencies [[tokyocabinet "1.24.3"]
+  :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
+             :dev {:dependencies [[tokyocabinet "1.24.3"]
                                   [unk "0.9.3"]]}}
+  :aliases {"testall" ["with-profile" "dev,default:dev,1.5,default" "test"]}
+  :repositories {"sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
+                                       :snapshots true
+                                       :releases {:checksum :fail :update :always}}}
   :checksum-deps true ;; Tired of accidentally running with old deps
   :hooks [leiningen.protobuf])
