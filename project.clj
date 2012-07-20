187f731da1964e0f81460fa0fa5b5611d78eccde
Anthony Grimes <i@raynes.me>
2012-07-19 19:25:31 -0500

    Temporarily back up to protobuf 0.6.0-beta18.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
                  [ego "0.1.7"]
                  [slingshot "0.10.3"]
                  [org.clojure/core.match "0.2.0-alpha9"]
-                 [protobuf "0.6.0-beta19"]]
+                 [protobuf "0.6.0-beta18"]]
   :plugins [[lein-protobuf "0.2.0-beta3"]]
   :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
              :dev {:dependencies [[tokyocabinet "1.24.3"]
