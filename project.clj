cff5e26f4420f5248b9542f60935dfed706f0422
Justin Balthrop <git@justinbalthrop.com>
2012-09-26 16:59:14 -0700

    update dependencies

--- project.clj
+++ project.clj
@@ -14,10 +14,10 @@
                  [ego "0.1.7"]
                  [slingshot "0.10.3"]
                  [org.clojure/core.match "0.2.0-alpha9"]
-                 [protobuf "0.6.2-alpha1"]]
-  :plugins [[lein-protobuf "0.2.0"]]
+                 [protobuf "0.6.2-alpha2"]]
+  :plugins [[lein-protobuf "0.2.1"]]
   :profiles {:1.5 {:dependencies [[org.clojure/clojure "1.5.0-master-SNAPSHOT"]]}
-             :dev {:dependencies [[tokyocabinet "1.24.4"]
+             :dev {:dependencies [[tokyocabinet "1.24.6"]
                                   [unk "0.9.3"]]}}
   :aliases {"testall" ["with-profile" "dev,default:dev,1.5,default" "test"]}
   :repositories {"sonatype-snapshots" {:url "http://oss.sonatype.org/content/repositories/snapshots"
