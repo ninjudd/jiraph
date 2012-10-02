3f8bd909ff1de8d0fc93b09c7b3f9a477e13a969
Alan Malloy <alan@malloys.org>
2012-10-02 16:33:30 -0700

    Switch to SNAPSHOT version while hacking

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jiraph "0.8.0"
+(defproject jiraph "0.8.1-SNAPSHOT"
   :description "embedded graph db library for clojure"
   :url "https://github.com/flatland/jiraph"
   :license {:name "Eclipse Public License - v 1.0"
