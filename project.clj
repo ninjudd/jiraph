f35291b2634a498a3c2e8d49641a2e2753fce874
Alan Malloy <alan@malloys.org>
2012-11-02 16:56:59 -0700

    0.8.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject jiraph "0.8.3-SNAPSHOT"
+(defproject jiraph "0.8.3"
   :description "embedded graph db library for clojure"
   :url "https://github.com/flatland/jiraph"
   :license {:name "Eclipse Public License - v 1.0"
