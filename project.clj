eb2a85a4c69d5ecc1a19f3dac0c0096b0181acab
Alan Malloy <alan@malloys.org>
2012-11-16 18:07:57 -0800

    Release 0.9.1 with fixed subseq handling

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.flatland/jiraph "0.9.1-SNAPSHOT"
+(defproject org.flatland/jiraph "0.9.1"
   :description "embedded graph db library for clojure"
   :url "https://github.com/flatland/jiraph"
   :license {:name "Eclipse Public License - v 1.0"
