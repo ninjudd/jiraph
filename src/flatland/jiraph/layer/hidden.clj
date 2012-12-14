75bf3d553fdae95a0c22500798f198ec2e76e50e
Alan Malloy <alan@malloys.org>
2012-12-14 11:56:55 -0800

    0.9.5: Add a HiddenLayer wrapper

---
+++ src/flatland/jiraph/layer/hidden.clj
@@ -0,0 +1,15 @@
+(ns flatland.jiraph.layer.hidden
+  (:use flatland.jiraph.wrapped-layer)
+  (:require [flatland.jiraph.graph :as graph :refer [update-in-node]]
+            [flatland.jiraph.layer :as layer]))
+
+(defwrapped HiddenLayer [layer]
+  layer/Schema
+  (schema [this id] nil))
+
+(defn make
+  "Wrap a layer such that it never reports a schema for any node-id.
+   This has the effect that code looking for the layers that apply to a given
+   node-id will act as if this layer doesn't exist."
+  [layer]
+  (HiddenLayer. layer))
