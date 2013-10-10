df0ffdd4b336a1eff30b15486f99cb57712818b4
Alan Malloy <alan@malloys.org>
2013-10-09 17:45:40 -0700

    Update useful

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "https://github.com/flatland/jiraph"
   :license {:name "Eclipse Public License - v 1.0"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/core.match "0.2.0-alpha11"]
                  [org.flatland/masai "0.8.0"]
-                 [org.flatland/useful "0.9.2"]
+                 [org.flatland/useful "0.10.4"]
                  [org.flatland/cereal "0.3.0"]
                  [org.flatland/ordered "1.4.0"]
                  [org.flatland/schematic "0.1.0"]
